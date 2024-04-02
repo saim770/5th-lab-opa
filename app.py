9a49e69d237d77a66050f76c15d873bf0200edee
Ubuntu <ubuntu@ip-172-31-27-79.eu-west-1.compute.internal>
2024-04-02 04:43:08 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!........'
+    return 'Hello, World!'
+
+@app.route('/api/data', methods=['GET', 'POST'])
+def api_data():
+    if request.method == 'GET':
+        return {"message": "GET request to /api/data successful."}
+    else:
+        return {"message": "This method is not allowed."}, 405
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
